e80252f8026ffc8534fab0a51fe8a2bf27111d9a
Andrey <a>
2018-12-28 19:56:44 +0300

    commit 1

---
+++ src/main/java/ru/manyatkin/SpringTest2/SpringTest2Application.java
@@ -0,0 +1,14 @@
+package ru.manyatkin.SpringTest2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTest2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringTest2Application.class, args);
+	}
+
+}
+
